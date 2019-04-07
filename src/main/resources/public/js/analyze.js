7dc384d2dfcb416818b729a83299f3255542eccd
ifs.sroy@gmail.com <ifs.sroy@gmail.com>
2019-04-07 19:59:07 -0400

    push changes for benchmark

--- src/main/resources/public/js/analyze.js
+++ src/main/resources/public/js/analyze.js
@@ -18,6 +18,7 @@ if (urlStr != null) {
         }
         $(".main-action input[name='url']").val(url);
         generateReport(url, globalData.strategy, globalData.mainAPI);
+        loadDarkMode();
     });
 }
 //031319124857622
