af3d430680b5c9bf6007d27d4d1b8a07477002b9
ifs.sroy@gmail.com <ifs.sroy@gmail.com>
2019-04-18 01:54:16 -0400

    add bar chart

---
+++ globalDependenciesEmbed.js
@@ -0,0 +1,15 @@
+HTTP/1.1 200 OK
+Content-Type: application/javascript
+Content-Length: 556717
+Connection: keep-alive
+Date: Thu, 18 Apr 2019 05:22:22 GMT
+Last-Modified: Wed, 17 Apr 2019 12:44:20 GMT
+ETag: "b2f9beeb31d16b3b79985cf80a311bcf"
+Cache-Control: public, max-age=36000
+Accept-Ranges: bytes
+Server: AmazonS3
+Vary: Accept-Encoding
+X-Cache: Miss from cloudfront
+Via: 1.1 d56db0d65906a3edce526dc6600d65c6.cloudfront.net (CloudFront)
+X-Amz-Cf-Id: A-V0zgZCKGgagSVMOrY4OmyfnwM1ehGcD-iWmyVXHS8YKIp9L_0evg==
+
